b385ba49b9da62e798344118750f9549272e419f
William Borromeo <drborromeo@up.edu.ph>
2020-01-29 14:38:37 +0800

    feat: added toast button + functionality, count button, & textview

--- app/src/main/java/com/example/toastapp/MainActivity.kt
+++ app/src/main/java/com/example/toastapp/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.toastapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showToast(view: View) {
+        val toast: Toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT)
+
+        toast.show()
+    }
+
+    fun countUp(view: View) {
+
     }
 }
